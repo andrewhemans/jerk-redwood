d83cfa57b02bcc0632a63125f8b798e3e8121cf1
andrewhemans <andrewhemans@gmail.com>
2020-04-01 20:53:59 -0700

    added pages and layout

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import MainLayout from 'src/layouts/MainLayout'
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+      <div>
+        <h1>HomePage</h1>
+        <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+      </div>
+    </MainLayout>
+  )
+}
+
+export default HomePage
